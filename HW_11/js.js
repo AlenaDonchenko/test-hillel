f012ca39cc0c3954493f79409c807b4961271eb9
AlenaDonchenko <126982282+AlenaDonchenko@users.noreply.github.com>
2023-07-30 20:56:31 +0200

    change code

--- HW_11/js.js
+++ HW_11/js.js
@@ -2,13 +2,13 @@ const array = ["a", 54, 12, 15, 4, "b"];
 
 function removeElement(array, element) {
 const indexElem = array.indexOf(element)
-    for (let i = 0; i < array.length; i++) {
-      if (array[i] === element){
+    // for (let i = 0; i < array.length; i++) {
+    //   if (array[i] === element){
          array.splice(indexElem,1);
-      }
-    }
+      // }
+    // }
     return array;
 }
 
-console.log(removeElement(array, 54));
+console.log(removeElement(array, 'a'));
 
