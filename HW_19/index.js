555b1f9f1f954d94fb5358ecf0342d31a96f569a
AlenaDonchenko <126982282+AlenaDonchenko@users.noreply.github.com>
2023-08-06 16:56:44 +0200

    work with objects

--- HW_19/index.js
+++ HW_19/index.js
@@ -1,30 +1,28 @@
- class Person {
-    constructor (name, age) {
+ function Person (name, age) {
      this.name = name;
      this.age = age;
+     this.getInfoPerson = function () {
+             console.log(`Імя: ${this.name}, вік: ${this.age}`)
+         }
  }
- getInfoPerson(){
-         console.log(`Імя: ${this.name}, вік: ${this.age}`)
- }
- }
- class Car {
-     constructor(brand, model, year, color, owner) {
+ function Car (brand, model, year, color, owner) {
          this.name = brand;
          this.model = model;
          this.year = year;
          this.color = color;
          this.owner = owner;
-     }
-     setOwner(owner) {
-         if (owner.age > 18) {
-             this.owner = owner;
-         } else {
-             console.log('Вік власника повинен бути > 18 років. ')
-         }
-     }
-     getInfoCar() {
+
+     this.setOwner = function (owner) {
+             if (owner.age >= 18) {
+                 this.owner = owner;
+             } else {
+                 console.log('Вік власника повинен бути > 18 років. ')
+             }
+         };
+
+     this.getInfoCar = function () {
          console.log(`Марка: ${this.name}, модель: ${this.model}, рік випуску: ${this.year}, колір: ${this.color}`);
-         if (this.owner){
+         if (this.owner) {
              console.log('Інформація про власника: ')
              this.owner.getInfoPerson()
          } else {
@@ -33,9 +31,9 @@
      }
  }
 
- const Person1 = new Person('Alex', 16);
- const Person2 = new Person('Lina', 27);
- const Person3 = new Person('Den', 23)
+ const Person1 = new Person('Alex', 25);
+ const Person2 = new Person('Lina', 17);
+ const Person3 = new Person('Den', 23);
 
  const car1 = new Car('Ford','Mondeo',2023, 'чорний');
  const car2 = new Car('Mazda','6', 2020, 'червоний');
@@ -46,9 +44,9 @@
  car3.setOwner(Person3);
 
 
- car1.getInfoCar();
- car2.getInfoCar();
- car3.getInfoCar();
+  car1.getInfoCar();
+  car2.getInfoCar();
+  car3.getInfoCar();
 
 
 
