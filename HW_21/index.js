3ddce808d17dbd014351c9e54c55a8496d4afce5
AlenaDonchenko <126982282+AlenaDonchenko@users.noreply.github.com>
2023-08-12 22:20:26 +0200

    work with object, Hamburger

--- HW_21/index.js
+++ HW_21/index.js
@@ -1,13 +1,27 @@
 class Hamburger {
+
     static SIZE_SMALL = {price: 50, calories: 20};
     static SIZE_BIG = {price: 100, calories: 40};
-    static STAFFING_CHEESE = {price:10, calories:20};
-    static STAFFING_SALAD = {price:20, calories:5};
-    static STAFFING_POTATO = {price:15, calories:10};
+    static STUFFING_CHEESE = {price: 10, calories: 20};
+    static STUFFING_SALAD = {price: 20, calories: 5};
+    static STUFFING_POTATO = {price: 15, calories: 10};
     static TOPPING_SPICE = {price: 15, calories: 0};
     static TOPPING_MAYO = {price: 20, calories: 5}
-    constructor() {
+
+constructor(size, stuffing, toppings) {
+        this.size = size;
+        this.stuffing = stuffing;
+        this.toppings = [];
+
+}
+    addTopping(topping) {
+        this.toppings.push(topping)
     }
+    calculateCalories(){
+        const total = this.size.calories + this.stuffing.calories;
+        return
+    }
+
 }
 
 
