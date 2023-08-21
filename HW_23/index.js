f23b75ba84257a5d75b9ac21c430d62bb211d4b2
AlenaDonchenko <126982282+AlenaDonchenko@users.noreply.github.com>
2023-08-21 20:36:58 +0200

    random photo

--- HW_23/index.js
+++ HW_23/index.js
@@ -1,12 +1,10 @@
-
 document.addEventListener("DOMContentLoaded", function() {
     getPhoto();
 });
 
 function getPhoto() {
     let imgElement = document.getElementById("random-photo");
-    const imageList = ["img1.jpg", "img2.jpg", "img3.jpg", "img4.jpg", "img5.jpg", "img6.jpg", "img7.jpg", "img8.jpg", "img9.jpg"];
+    const imageList = ["img_01.jpg", "img_02.jpg", "img_03.jpg", "img_04.jpg", "img_05.jpg", "img_06.jpg", "img_07.jpg", "img_08.jpg", "img_09.jpg"];
     imgElement.src = 'HW_23Img/' +  imageList[Math.floor(Math.random() * imageList.length)];
-
 }
 
