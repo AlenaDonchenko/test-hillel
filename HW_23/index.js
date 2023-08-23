e5aa6a23d5d82bc982a3c5f48c09ccb0d37a0fbd
AlenaDonchenko <126982282+AlenaDonchenko@users.noreply.github.com>
2023-08-23 17:46:06 +0200

    random photo, changed code

--- HW_23/index.js
+++ HW_23/index.js
@@ -1,6 +1,6 @@
-document.addEventListener("DOMContentLoaded", function() {
+/*document.addEventListener("DOMContentLoaded", function() {
     getPhoto();
-});
+});*/
 function getPhoto() {
     let imgElement = document.getElementById("random-photo");
     const imageList = ["img_01.jpg", "img_02.jpg", "img_03.jpg", "img_04.jpg", "img_05.jpg", "img_06.jpg", "img_07.jpg", "img_08.jpg", "img_09.jpg"];
