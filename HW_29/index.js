fe0f9f43981b214213ebbe0e5b54bc0ae60748c5
AlenaDonchenko <126982282+AlenaDonchenko@users.noreply.github.com>
2023-09-06 22:35:59 +0200

    working with events, website

--- HW_29/index.js
+++ HW_29/index.js
@@ -1,86 +1,21 @@
-// class User {
-//     constructor(name, lastname, birthday, gender, city, address, language) {
-//         this._name = name;
-//         this._lastname = lastname;
-//         this._birthday = birthday;
-//         this._gender = gender;
-//         this._city = city;
-//         this._address = address;
-//         this._language = language;
-//     }
-//
-//
-//     get name() {
-//         return this._name;
-//     }
-//
-//     set name(value) {
-//         this._name = value;
-//     }
-//
-//     get lastname() {
-//         return this._lastname;
-//     }
-//
-//     set lastname(value) {
-//         this._lastname = value;
-//     }
-//
-//     get birthday() {
-//         return this._birthday;
-//     }
-//
-//     set birthday(value) {
-//         this._birthday = value;
-//     }
-//
-//     get gender() {
-//         return this._gender;
-//     }
-//
-//     set gender(value) {
-//         this._gender = value;
-//     }
-//
-//     get city() {
-//         return this._city;
-//     }
-//
-//     set city(value) {
-//         this._city = value;
-//     }
-//
-//     get address() {
-//         return this._address;
-//     }
-//
-//     set address(value) {
-//         this._address = value;
-//     }
-//
-//     get language() {
-//         return this._language;
-//     }
-//
-//     set language(value) {
-//         this._language = value;
-//     }
-// }
-
 function showInfo() {
     const form = document.getElementById('registrationForm');
-    const textOutput = document.getElementById('textOutput');
+    const dataTable = document.getElementById("dataTable");
+    const table = document.querySelector("table");
+    table.innerHTML = '';
 
-    let outputText = "Введені данні:\n"
     for (let i = 0; i < form.elements.length - 1; i++) {
         const element = form.elements[i];
         if (element.type !== 'button') {
-            outputText += `${element.name}: ${element.value}\n`
+            const row = table.insertRow(-1);
+            const cell1 = row.insertCell(0);
+            const cell2 = row.insertCell(1);
+            cell1.textContent = element.name;
+            cell2.textContent = element.value;
         }
     }
-    // textOutput.querySelector("pre").textContent = outputText;
-    textOutput.textContent = outputText;
-    textOutput.classList.remove("hidden");
+    dataTable.classList.remove("hidden");
     form.classList.add("hidden");
+
 }
 
