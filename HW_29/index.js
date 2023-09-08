1abea2cb1b32270e1784f665ff5d004403fdf9b7
AlenaDonchenko <126982282+AlenaDonchenko@users.noreply.github.com>
2023-09-08 15:15:10 +0200

    working with object, events, form

--- HW_29/index.js
+++ HW_29/index.js
@@ -1,10 +1,9 @@
 class Person {
-
-    constructor(firstName, lastName, berstday, address, gender, city, languages) {
+    constructor(firstName, lastName, birthday, address, gender, city, languages) {
 
         this._firstName = firstName;
         this._lastName = lastName;
-        this._berstday = berstday;
+        this._berstday = birthday;
         this._gender = gender;
         this._city = city;
         this._address = address;
@@ -14,89 +13,71 @@ class Person {
     get firstName() {
         return this._firstName;
     }
-
     set firstName(value) {
         this._firstName = value;
     }
-
     get lastName() {
         return this._lastName;
     }
-
     set lastName(value) {
         this._lastName = value;
     }
-
-    get berstday() {
+    get birthday() {
         return this._berstday;
     }
-
-    set berstday(value) {
+    set birthday(value) {
         this._berstday = value;
     }
-
     get gender() {
         return this._gender;
     }
-
     set gender(value) {
         this._gender = value;
     }
-
     get city() {
         return this._city;
     }
-
     set city(value) {
         this._city = value;
     }
-
     get address() {
         return this._address;
     }
-
     set address(value) {
         this._address = value;
     }
-
     get languages() {
         return this._languages;
     }
-
     set languages(value) {
         this._languages = value;
     }
 }
 
-
 function showInfo() {
+    let tbody = document.getElementById('tbody');
     const form = document.getElementById('registrationForm');
     const dataTable = document.getElementById("dataTable");
     const table = document.querySelector("table");
 
-    table.innerHTML = '';
-
     let person = findPersonFromForm();
+    let tr = "<tr>";
 
-    console.log(person);
+    tr += "<td>" + person.firstName + "</td>" +
+        "<td>" + person.lastName + "</td>" +
+        "<td>" + person.birthday + "</td>" +
+        "<td>" + person.gender + "</td>" +
+        "<td>" + person.city + "</td>" +
+        "<td>" + person.address + "</td>" +
+        "<td>" + person.languages + "</td>" +
+        "</tr>";
 
-
-    for (let i = 0; i < form.elements.length - 1; i++) {
-        const element = form.elements[i];
-        if (element.type !== 'button') {
-            const row = table.insertRow(-1);
-            const cell1 = row.insertCell(0);
-            const cell2 = row.insertCell(1);
-            cell1.textContent = element.name;
-            cell2.textContent = element.value;
-        }
-    }
+    tbody.innerHTML += tr;
     dataTable.classList.remove("hidden");
     form.classList.add("hidden");
-}
 
+}
 function findValue(id) {
-    console.log(id)
     return document.getElementById(id).value;
 }
 
@@ -127,8 +108,7 @@ function findGender() {
         return document.getElementById('male').value;
 
     } else if (document.getElementById('female').checked) {
-        return document.getElementById('female').value
-
-
+        return document.getElementById('female').value;
+    }
 }
-}
+
